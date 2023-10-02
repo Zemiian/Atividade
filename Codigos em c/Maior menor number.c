1b8c7a32f6b644a664af9caf014159ae7b42dd37
Zemiian <143917828+Zemiian@users.noreply.github.com>
2023-10-02 10:35:50 -0300

    Update and rename Maior menor number.c to Codigos em c/Maior menor number.c

--- Codigos em c/Maior menor number.c
+++ Codigos em c/Maior menor number.c
@@ -9,45 +9,45 @@ int main()
     int num2 = 0;
     int num3 = 0;
 
-    printf("Digite o primeiro número\n");
+    printf("Digite o primeiro nÃºmero\n");
     scanf("%i", &num1);
 
     //fflush()
 
-    printf("Digite o segundo número\n");
+    printf("Digite o segundo nÃºmero\n");
     scanf("%i", &num2);
 
-    printf("Digite o segundo número\n");
+    printf("Digite o segundo nÃºmero\n");
     scanf("%i", &num3);
 
-    printf("\nPrimeiro número: %i" ,num1);
-    printf("\nSegundo número: %i" ,num2);
-    printf("\nTerceiro número: %i" ,num3);
+    printf("\nPrimeiro nÃºmero: %i" ,num1);
+    printf("\nSegundo nÃºmero: %i" ,num2);
+    printf("\nTerceiro nÃºmero: %i" ,num3);
 
     if(num1 > num2 && num1 > num3){
-    printf("\nMaior número: %i " ,num1);
+    printf("\nMaior nÃºmero: %i " ,num1);
     }
 
     if(num2 > num1 && num2 > num3){
-    printf("Maior número: %i" ,num2);
+    printf("Maior nÃºmero: %i" ,num2);
 
     }
 
     if (num3 > num1 && num3 > num2){
-    printf("Maior número:  %i" ,num3);    
+    printf("Maior nÃºmero:  %i" ,num3);    
     }
 
     if(num1 < num2 && num1 < num3){
-    printf("\nMenor número: %i " ,num1);
+    printf("\nMenor nÃºmero: %i " ,num1);
     }
 
     if(num2 < num1 && num2 < num3){
-    printf("Menor número: %i" ,num2);
+    printf("Menor nÃºmero: %i" ,num2);
 
     }
 
     if (num3 < num1 && num3 < num2){
-    printf("Menor número:  %i" ,num3);    
+    printf("Menor nÃºmero:  %i" ,num3);    
     }
 
 
