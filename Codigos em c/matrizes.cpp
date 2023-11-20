845d5d718303d0af621c01ebae5674dcd4e18740
Douglas <143917828+Zemiian@users.noreply.github.com>
2023-11-20 10:15:30 -0300

    Add files via upload

---
+++ Codigos em c/matrizes.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "portuguese");
+	
+char nomes [2][200];
+float notas [2][3];	
+int i,j,k,soma=0;	
+float media[3];
+	
+for(i = 0; i < 2; i++){
+	
+	printf("\n Digite o nome do aluno: ");
+	scanf("%s" ,&nomes[i]);
+	
+	for(j = 0; j < 3; j++){
+		printf("\n Digite a nota do aluno: ");
+		scanf("%f" ,&notas[i][j]);	
+		
+		soma += notas[i][j];
+	}
+	
+	media[i] = soma / j;	
+	soma = 0;
+	fflush(stdin);
+}
+
+system("cls");
+
+for(i = 0; i < 2; i++){
+	printf("\n \n Nome: %s" ,nomes[i]);
+		
+	for(j = 0; j < 3; j++){
+		printf("\n \n Notas: %1.f" ,notas[i][j]);	
+	}	
+	
+printf("\n \n Média: %1.f" ,media[i]);
+	
+}
+
+
+
+
+
+	
+return 0;	
+	
+}
